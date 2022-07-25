9361c42494100feb8678714efd4d6aeb773a74e5
msap2002 <109910953+msap2002@users.noreply.github.com>
2022-07-25 23:20:31 +0530

    Time: 7 ms (70.73%), Space: 45.1 MB (68.79%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int romanToInt(String s) {
+        int n = s.length();
+        int res = 0;
+        for(int i = 0;i < n - 1;i++){
+            int a = getValue(s.charAt(i));
+            int b = getValue(s.charAt(i + 1));
+            if(a < b){
+                res = res - a;
+            }
+            else{
+                res = res + a;
+            }
+        }
+        res = res + getValue(s.charAt(n - 1));
+        return res;
+    }
+    public int getValue(char c){
+        if(c=='I') return 1;
+        else if(c=='V') return 5;
+        else if(c=='X') return 10;
+        else if(c=='L') return 50;
+        else if(c=='C') return 100;
+        else if(c=='D') return 500;
+        else return 1000;
+    }
+}
