6deb1c452515a9491e07bf0af8a51cff08a0fc38
SAPTARSHI MUKHERJEE <109910953+msap2002@users.noreply.github.com>
2022-08-02 00:44:49 +0530

    Time: 10 ms (13.49%), Space: 51.6 MB (22.92%) - LeetHub

---
+++ 268-missing-number/268-missing-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 0;i < nums[nums.length - 1];i++){
+            if(nums[i] != i)
+                return i;
+        }
+        return nums[nums.length - 1] + 1;
+    }
+}
