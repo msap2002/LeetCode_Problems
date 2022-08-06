420bffe8ee4acd57855dff444ed191bd11e4c567
SAPTARSHI MUKHERJEE <109910953+msap2002@users.noreply.github.com>
2022-08-07 00:16:58 +0530

    Time: 5 ms (37.74%), Space: 46.3 MB (71.34%) - LeetHub

---
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root == null && subRoot == null)
+            return true;
+        if(root == null || subRoot == null)
+            return false;
+        boolean ans = isMatching(root,subRoot);
+        boolean result = isSubtree(root.left,subRoot);
+        boolean result2 = isSubtree(root.right,subRoot);
+        return ans||result||result2;
+    }
+    public boolean isMatching(TreeNode root,TreeNode subRoot){
+        if(root == null && subRoot == null)
+            return true;
+        if(root != null && subRoot != null && root.val != subRoot.val)
+            return false;
+        if(root != null && subRoot == null)
+            return false;
+        if(root == null && subRoot != null)
+            return false;
+        boolean result1 = isMatching(root.left,subRoot.left);
+        boolean result2 = isMatching(root.right,subRoot.right);
+        return (result1 & result2);
+    }
+}
