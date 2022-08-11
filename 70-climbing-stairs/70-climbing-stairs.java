3f0a2aa5aad1611eb5eb761c8a0124c14093626c
SAPTARSHI MUKHERJEE <109910953+msap2002@users.noreply.github.com>
2022-08-11 11:07:33 +0530

    Time: 1 ms (11.11%), Space: 41.5 MB (7.63%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        int dp[] = new int[n + 1];
+        if(n == 0||n == 1||n == 2||n == 3)
+            return n;
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+        System.out.println(dp.length);
+        for(int i = 4;i < dp.length;i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
