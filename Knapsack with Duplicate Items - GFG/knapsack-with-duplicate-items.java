6910b45fe24bd7a8a5ca6ef8a4762c81694dff4b
SAPTARSHI MUKHERJEE <109910953+msap2002@users.noreply.github.com>
2022-12-05 12:09:20 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.java
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.java
@@ -39,11 +39,14 @@ class Solution{
         int dp[][] = new int[N + 1][W + 1];
         for(int i = 0;i < N + 1;i++){
             for(int j = 0;j < W + 1;j++){
-                if(i == 0||j == 0){
+                if(i == 0||j == 0)
                     dp[i][j] = 0;
-                }
-                else if(wt[i - 1] <= j){
-                    dp[i][j] = Math.max((val[i - 1] + dp[i][j - wt[i - 1]]),dp[i - 1][j]);
+            }
+        }
+        for(int i = 1;i < N + 1;i++){
+            for(int j = 1;j < W + 1;j++){
+                if(wt[i - 1] <= j){
+                    dp[i][j] = (int)Math.max(val[i - 1] + dp[i][j - wt[i - 1]],dp[i - 1][j]);
                 }
                 else{
                     dp[i][j] = dp[i - 1][j];
