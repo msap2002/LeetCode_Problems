a4bf27749ae887cd30220562e08c3aa720e13db2
SAPTARSHI MUKHERJEE <109910953+msap2002@users.noreply.github.com>
2022-12-07 00:55:32 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.java
+++ Perfect Sum Problem - GFG/perfect-sum-problem.java
@@ -30,21 +30,24 @@ class Solution{
 	public int perfectSum(int arr[],int n, int sum) 
 	{ 
 	    // Your code goes here
-	    int mod = (int)1e9 + 7;
 	    int dp[][] = new int[n + 1][sum + 1];
-	    dp[0][0] = 1;
-	    for(int i = 0;i < n;i++){
-	        for(int j = 0;j < sum;j++){
+	    int mod = (int)1e9 + 7;
+	    for(int i = 0;i < n + 1;i++){
+	        for(int j = 0;j < sum + 1;j++){
+	            if(i == 0)
+	              dp[i][j] = 0;
 	            if(j == 0)
 	              dp[i][j] = 1;
 	        }
 	    }
 	    for(int i = 1;i < n + 1;i++){
 	        for(int j = 0;j < sum + 1;j++){
-	            if(arr[i - 1] <= j)
-	              dp[i][j] = (dp[i - 1][j - arr[i - 1]] + dp[i - 1][j]) % mod;
-	            else
-	              dp[i][j] = (dp[i - 1][j]) % mod;
+	            if(arr[i - 1] <= j){
+	                dp[i][j] = (dp[i - 1][j - arr[i - 1]] + dp[i - 1][j]) % mod;
+	            }
+	            else{
+	                dp[i][j] = (dp[i - 1][j]) % mod;
+	            }
 	        }
 	    }
 	    return dp[n][sum] % mod;
