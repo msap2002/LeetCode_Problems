9c09522c358dc88809fa2afb7590e0ab9a6589d5
SAPTARSHI MUKHERJEE <109910953+msap2002@users.noreply.github.com>
2022-12-08 23:27:03 +0530

    Added solution - LeetHub

--- Longest Repeating Subsequence - GFG/longest-repeating-subsequence.java
+++ Longest Repeating Subsequence - GFG/longest-repeating-subsequence.java
@@ -45,12 +45,10 @@ class Solution
         }
         for(int i = 1;i < m + 1;i++){
             for(int j = 1;j < n + 1;j++){
-                if(str1.charAt(i - 1) == str2.charAt(j - 1) && (i != j)){
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                }
-                else{
-                    dp[i][j] = (int)Math.max(dp[i - 1][j],dp[i][j - 1]);
-                }
+                if(str1.charAt(i - 1) == str2.charAt(j - 1) && (i != j))
+                  dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                  dp[i][j] = (int)Math.max(dp[i - 1][j],dp[i][j - 1]);
             }
         }
         return dp[m][n];
