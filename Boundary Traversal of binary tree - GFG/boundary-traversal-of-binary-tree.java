25a0cc82affd5a65a86bdc6519c231e31420f8d1
SAPTARSHI MUKHERJEE <109910953+msap2002@users.noreply.github.com>
2023-03-12 14:53:17 +0530

    Added solution - LeetHub

---
+++ Boundary Traversal of binary tree - GFG/boundary-traversal-of-binary-tree.java
@@ -0,0 +1,162 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node  
+{ 
+    int data; 
+    Node left, right; 
+   
+    public Node(int d)  
+    { 
+        data = d; 
+        left = right = null; 
+    } 
+}
+
+class GFG
+{
+    static Node buildTree(String str)
+    {
+        // Corner Case
+        if(str.length() == 0 || str.equals('N'))
+            return null;
+        String[] s = str.split(" ");
+        
+        Node root = new Node(Integer.parseInt(s[0]));
+        Queue <Node> q = new LinkedList<Node>();
+        q.add(root);
+        
+        // Starting from the second element
+        int i = 1;
+        while(!q.isEmpty() && i < s.length)
+        {
+              // Get and remove the front of the queue
+              Node currNode = q.remove();
+        
+              // Get the current node's value from the string
+              String currVal = s[i];
+        
+              // If the left child is not null
+              if(!currVal.equals("N")) 
+              {
+        
+                  // Create the left child for the current node
+                  currNode.left = new Node(Integer.parseInt(currVal));
+        
+                  // Push it to the queue
+                  q.add(currNode.left);
+              }
+        
+              // For the right child
+              i++;
+              if(i >= s.length)
+                  break;
+              currVal = s[i];
+        
+              // If the right child is not null
+              if(!currVal.equals("N")) 
+              {
+        
+                  // Create the right child for the current node
+                  currNode.right = new Node(Integer.parseInt(currVal));
+        
+                  // Push it to the queue
+                  q.add(currNode.right);
+              }
+              
+              i++;
+        }
+    
+        return root;
+    }
+    
+    public static void main(String args[]) throws IOException {
+    
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t>0)
+        {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            
+            Solution T = new Solution();
+            
+            ArrayList <Integer> res = T.boundary(root);
+            for (Integer num : res) System.out.print (num + " ");
+            System.out.println();
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+// class Node  
+// { 
+//     int data; 
+//     Node left, right; 
+   
+//     public Node(int d)  
+//     { 
+//         data = d; 
+//         left = right = null; 
+//     } 
+// }
+
+class Solution
+{
+	ArrayList <Integer> boundary(Node node)
+	{
+	    ArrayList<Integer>ans = new ArrayList<>();
+	    if (isLeaf(node) == false) ans.add(node.data);
+	    leftBoundary(node,ans);
+	    addLeaves(node,ans);
+	    rightBoundary(node,ans);
+	    return ans;
+	}
+	public boolean isLeaf(Node node){
+	    if(node.left == null && node.right == null)
+	      return true;
+	    return false;
+	}
+	public void leftBoundary(Node node,ArrayList<Integer>ans){
+	    Node temp = node.left;
+	    while(temp != null){
+	        if(isLeaf(temp) == false) ans.add(temp.data);
+	        if(temp.left != null)
+	          temp = temp.left;
+	        else
+	          temp = temp.right;
+	    }
+	}
+	public void rightBoundary(Node node,ArrayList<Integer>ans){
+	    Node temp = node.right;
+	    ArrayList<Integer>temp1 = new ArrayList<>();
+	    while(temp != null){
+	        if(isLeaf(temp) == false) temp1.add(temp.data);
+	        if(temp.right != null)
+	          temp = temp.right;
+	        else
+	          temp = temp.left;
+	    }
+	    for(int i = temp1.size() - 1;i >= 0;i--){
+	        ans.add(temp1.get(i));
+	    }
+	}
+	public void addLeaves(Node node,ArrayList<Integer>ans){
+	    if(isLeaf(node)){
+	        ans.add(node.data);
+	        return;
+	    }
+	    if(node.left != null){
+	        addLeaves(node.left,ans);
+	    }
+	    if(node.right != null){
+	        addLeaves(node.right,ans);
+	    }
+	}
+}
