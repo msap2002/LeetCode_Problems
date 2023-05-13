d4e5b38d4dfa92b7875419eb9d869f1061e4cf01
SAPTARSHI MUKHERJEE <109910953+msap2002@users.noreply.github.com>
2023-05-13 22:41:49 +0530

    Added solution - LeetHub

--- Geek Jump - GFG/geek-jump.java
+++ Geek Jump - GFG/geek-jump.java
@@ -33,20 +33,18 @@ class GFG{
 class Solution{
     public int minimumEnergy(int arr[],int N){
         //code here
-        int dp[] = new int[N + 1];
-        for(int i = 0;i < N;i++)
-          dp[i] = -1;
-        return minimumEnergyMem(N - 1,arr,N,dp);
+        return f(N - 1,arr,N);
     }
-    public int minimumEnergyMem(int index,int arr[],int N,int dp[]){
-        if(index == 0)
-          return 0;
-        if(dp[index] != -1)
-          return dp[index];
-        int left = minimumEnergyMem(index - 1,arr,N,dp) + (int)Math.abs(arr[index] - arr[index - 1]);
-        int right = Integer.MAX_VALUE;
-        if(index > 1)
-          right = minimumEnergyMem(index - 2,arr,N,dp) + (int)Math.abs(arr[index] - arr[index - 2]);
-        return dp[index] = (int)Math.min(left,right);
+    public int f(int index,int arr[],int N){
+        int dp[] = new int[N + 1];
+        dp[0] = 0;
+        for(int i = 1;i < N;i++){
+            int fs = dp[i - 1] + (int)Math.abs(arr[i] - arr[i - 1]);
+            int ss = Integer.MAX_VALUE;
+            if(i > 1)
+              ss = dp[i - 2] + (int)Math.abs(arr[i] - arr[i - 2]);
+            dp[i] = (int)Math.min(fs,ss);
+        }
+        return dp[N - 1];
     }
 }
