486eee8e3e03ac8cc624ee5c7bfb8eef6a068451
SAPTARSHI MUKHERJEE <109910953+msap2002@users.noreply.github.com>
2023-08-12 19:46:06 +0530

    Added solution - LeetHub

--- Reverse a String - GFG/reverse-a-string.java
+++ Reverse a String - GFG/reverse-a-string.java
@@ -32,10 +32,10 @@ class Reverse
     public static String reverseWord(String str)
     {
         // Reverse the string str
-        String revStr = "";
+        String res = "";
         for(int i = str.length() - 1;i >= 0;i--){
-            revStr += str.charAt(i);
+            res = res + str.charAt(i);
         }
-        return revStr;
+        return res;
     }
 }
