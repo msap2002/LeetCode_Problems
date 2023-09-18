4be7e2d2a25a4a6dcf20012ae881ad5db9c78f91
SAPTARSHI MUKHERJEE <109910953+msap2002@users.noreply.github.com>
2023-09-18 23:16:33 +0530

    Added solution - LeetHub

--- A Simple Fraction - GFG/a-simple-fraction.java
+++ A Simple Fraction - GFG/a-simple-fraction.java
@@ -32,71 +32,31 @@ class Solution
     public String  fractionToDecimal(int numerator, int denominator)
     {
         // code here
-        // double res = (double)numerator / (double)denominator;
-        // String ans = "";
-        // ans += Double.toString(res);
-        // int i = 0;
-        // while(ans.charAt(i) != '.'){
-        //     ans = ans + ans.charAt(i);
-        //     i++;
-        // }
-        // String res1 = ans.substring(i + 1);
-        // HashMap<Character,Integer>map = new HashMap<>();
-        // for(int x = 0;x < res1.length();x++){
-        //     map.put(res1.charAt(x),map.getOrDefault(res1.charAt(x),0) + 1);
-        // }
-        // String ans2 = "";
-        // for(Map.Entry<Character,Integer>entry : map.entrySet()){
-        //     if(entry.getValue() > 1){
-        //         ans2 += "(" + Integer.toString(entry.getKey()) + ")";
-        //     }
-        //     else{
-        //         ans2 += Integer.toString(entry.getKey());
-        //     }
-        // }
-        // ans = ans + "." + ans2;
-        // return ans;
-         if (numerator == 0) {
-            return "0";
-        }
-
-        StringBuilder ans = new StringBuilder();
-        // Handle sign
-        if ((numerator < 0) || (denominator < 0)) {
-            ans.append("-");
-        }
-
-        long num = Math.abs((long) numerator);
-        long den = Math.abs((long) denominator);
-
-        // Integer part
-        ans.append(num / den);
-        num %= den;
-
-        // If no fractional part, return now
-        if (num == 0) {
-            return ans.toString();
-        }
-
-        ans.append(".");
-
-        Map<Long, Integer> map = new HashMap<>();
-        while (num != 0) {
-            // If we encounter the same remainder again, it's a recurring decimal
-            if (map.containsKey(num)) {
+        if(numerator == 0)
+           return "0";
+        StringBuilder sb = new StringBuilder();
+        if(numerator < 0||denominator < 0)
+           sb.append("-");
+        long num = Math.abs((long)(numerator));
+        long deno = Math.abs((long)(denominator));
+        sb.append(num/deno);
+        num %= deno;
+        if(num == 0)
+           return sb.toString();
+        sb.append(".");
+        Map<Long,Integer>map = new HashMap<>();
+        while(num != 0){
+            if(map.containsKey(num)){
                 int index = map.get(num);
-                ans.insert(index, "(");
-                ans.append(")");
+                sb.insert(index,"(");
+                sb.append(")");
                 break;
             }
-
-            map.put(num, ans.length());
-
+            map.put(num,sb.length());
             num *= 10;
-            ans.append(num / den);
-            num %= den;
+            sb.append(num/deno);
+            num %= deno;
         }
-
-        return ans.toString();
+        return sb.toString();  
     }
 }
